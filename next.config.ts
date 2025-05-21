238031e710f013b6d6142e5f9c68f166896f4329
Sergio Laín <sergio.lac@pm.me>
2025-05-21 21:27:16 +0300

    frontend finished

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: [
+      "librarybookrent.s3.eu-north-1.amazonaws.com", // ✅ This one matches
       "img.freepik.com",
       "www.freepik.com",
       "images.pexels.com",
       "librarybookingwebsite.s3.us-east-1.amazonaws.com",
-    ], // Add the domain here
+    ],
   },
 };
 
